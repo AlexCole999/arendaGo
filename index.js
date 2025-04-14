a5decad9567dc2f46ba8edb9eee9aa5b861bd47f
Leo <62186360+AlexCole999@users.noreply.github.com>
2025-04-14 19:36:59 +0500

    init 2nd iteration

--- index.js
+++ index.js
@@ -24,12 +24,12 @@ app.use(invitationRoutes);
 app.use(express.static('uploads'));//директория с отдачей файлов
 app.use(express.static('uploadsAdmin'));//директория с отдачей файлов
 
-let host = 'mongodb+srv://leonidworkuz:kKf3RCKhTh4Zpu3p@clusterarendagotest.jkls2x2.mongodb.net/?retryWrites=true&w=majority&appName=ClusterArendaGoTest'
+// let host = 'mongodb+srv://leonidworkuz:kKf3RCKhTh4Zpu3p@clusterarendagotest.jkls2x2.mongodb.net/?retryWrites=true&w=majority&appName=ClusterArendaGoTest'
 
 // Подключение к MongoDB
-mongoose.connect(host)
+mongoose.connect('mongodb://localhost:27017/arendaGo')
   .then(() => {
-    console.log('Подключено к MongoDB');
+    console.log('Подключено к MongoDB на localhost');
   }).catch((err) => {
     console.error('Ошибка подключения к MongoDB:', err);
   });
